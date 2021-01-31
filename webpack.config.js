116ab3a372adc1ce63bab365320bcdc184b55138
James Johnson <james.johnson280@gmail.com>
2021-01-31 17:47:07 -0500

    Add dist cleaning

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, './src/index.jsx'),
@@ -21,6 +22,7 @@ module.exports = {
     filename: 'bundle.js'
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'Hello React!',
       template: path.resolve(__dirname, './static/index.html'),
