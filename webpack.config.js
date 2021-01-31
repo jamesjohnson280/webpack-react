8388b8d2e93019f8201d6fdd0c5d5879aa48ac39
James Johnson <james.johnson280@gmail.com>
2021-01-31 17:19:24 -0500

    Set up React

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, './src/index.js'),
+  entry: path.resolve(__dirname, './src/index.jsx'),
   module: {
     rules: [
       {
-        test: /\.(js)$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ['babel-loader']
       }
     ]
   },
   resolve: {
-    extensions: ['*', '.js']
+    extensions: ['*', '.js', 'jsx']
   },
   output: {
     path: path.resolve(__dirname, './dist'),
